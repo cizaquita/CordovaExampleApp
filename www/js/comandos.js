1103f121ebdfb1798e421af215a98eaff446b3b2
Cristian Ivan Izaquita <cristian.izaquita@gmail.com>
2015-07-14 10:25:08 -0500

    Update code, create new JSON files

--- www/js/comandos.js
+++ www/js/comandos.js
@@ -12,3 +12,19 @@ var x;
 		var x=$("#menu");
 		x.toggle("slow");//slidetoggle
 	}
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '645549865545526',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
